554305620935745e7b3a406d3b3a594adad57970
fisker <lionkay@gmail.com>
2020-02-03 13:19:12 +0800

    Add success test

--- test.js
+++ test.js
@@ -2,6 +2,12 @@ const path = require('path')
 
 test('should throws', () => {
   expect(() => {
+    require.resolve('foo/node_modules/bar');
+  }).toThrow()
+})
+
+test('should throws too', () => {
+  expect(() => {
     require.resolve('foo/node_modules/bar', {paths: [process.cwd()]});
   }).toThrow()
 })
